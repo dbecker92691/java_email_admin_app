9ece71f90e77909aecdac52e0b7e4ff6b80d1e61
dbecker92691 <dbecker92691@gmail.com>
2019-02-05 12:13:22 -0700

    built a setDepartment method

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -13,14 +13,40 @@ public class Email {
 	// Constructor to recieve first name and last name
 
 	public Email(String firstName, String lastName) {
+
 		// vv class var || vv local var
 		this.firstName = firstName;
 		this.lastName = lastName;
+		System.out.println("Email created " + this.firstName + " " + this.lastName);
 
-		System.out.println("Email created" + this.firstName + " " + this.lastName);
+		// call a method asking for the department & return the department
+		// private class department calls setDepartment method
+		this.department = setDepartment();
+
+
 	}
 
 	// Ask for the department
+	private String setDepartment() {
+		System.out.print("DEPARTMENT CODES:\n1 for sales Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter Department Code: ");
+
+
+
+		// creating a scanner object to read the "IT department's input of the above code"
+		Scanner in = new Scanner(System.in);
+		// this portion reads the integer entered in the command line
+		int depChoice = in.nextInt();
+
+
+
+		if (depChoice == 1) { return "sales"; } 
+		else if (depChoice == 2) { return "development"; } 
+		else if (depChoice == 3) { return "accounting"; } 
+		else { return ""; }
+
+	}
+
+
 
 	// Generate a random password
 
