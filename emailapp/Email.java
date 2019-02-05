61ca59f0587882a25e6cb88da77d9bcbe8f8b1f3
dbecker92691 <dbecker92691@gmail.com>
2019-02-05 10:12:30 -0700

    created Email Class outline

--- emailapp/Email.java
+++ emailapp/Email.java
@@ -2,4 +2,25 @@ package emailapp;
 
 public class Email {
 	
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+
+
+	// Constructor to recieve first name and last name
+
+	// Ask for the department
+
+	// Generate a random password
+
+	// Set the mailbox capacity
+
+	// Set the alternate email
+
+	// Change the password
+
+	
 }
